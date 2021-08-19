6bdc9bf32d72f3bfb2fc709f8b90438a674f2b9d
Xia Hua <huaxia@Xias-MacBook-Air.local>
2021-08-19 17:03:45 +0800

    First version of application moved into GitHub

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// GET home page.
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
+});
+
+module.exports = router;
+
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log(1+1);
+var x = 1;
+let y = 1;
+const z = 1;
+console.log(x);
+console.log(y);
+console.log(z);
